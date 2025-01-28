eb41443b270ca6f4a676560db937c30b0b6c0f9c
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2025-01-28 07:56:18 -0700

    Switch reposync code to script

---
+++ reposync.sh
@@ -0,0 +1,14 @@
+#1/bin/bash
+echo "Collecting $1 RPMs"
+reposync --download-metadata \
+  --repo=epel \
+  -p /mnt/$1 > $1.log &
+  pid=$!
+echo "Process $pid is running"
+while kill -0 $pid 2>/dev/null; do
+  free -h | awk 'NR==2 {print $4}'
+  df -h /mnt | awk 'NR==2 {print $4}'
+  tail -n 1 $1.log
+  sleep 10
+done
+echo "Process $pid is Complete"
