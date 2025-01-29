93b87a13c5800753a0c1685ea6e2a52811071445
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2025-01-28 23:38:11 -0700

    Cleanup output and test next repo

--- reposync.sh
+++ reposync.sh
@@ -5,14 +5,14 @@ reposync --download-metadata \
   -p /mnt/$1 > $1.log &
   pid=$!
 echo "Process $pid is running"
+
 while kill -0 $pid 2>/dev/null; do
-  echo -e "Free Mem:\t$(free -h | awk 'NR==2 {print $4}')" \
-    "\t\tDisk Space:\t$(df -h /mnt | awk 'NR==2 {print $4}')" \
-    "\n$(tail -n 1 $1.log)"
-  # df -h /mnt | awk 'NR==2 {print $4}'
-  # tail -n 1 $1.log
   sleep 15
+  echo -e "Free Mem: $(free -h | awk 'NR==2 {print $4}')" \
+    "\tFree Space: $(df -h /mnt | awk 'NR==2 {print $4}')" \
+    "\n$(tail -n 1 $1.log)"
 done
+
 echo "Process $pid is Complete"
-echo -e "\nListing:\n$(ls -l /mnt/$1)\n"
-echo -e "\nConsumption:\n$(du -sh /mnt/$1)\n"
+echo -e "\nListing:\n$(ls -l /mnt/$1)"
+echo -e "\nConsumption:\n$(du -sh /mnt/$1)"
