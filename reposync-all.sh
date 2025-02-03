9542502873fba34825386700523e45e53d7a9e30
John Pappas <John.Pappas@JacobsFederal.com>
2025-02-03 14:24:26 -0700

    rename reposync file for all

---
+++ reposync-all.sh
@@ -0,0 +1,18 @@
+#1/bin/bash
+echo "Collecting $1 RPMs"
+reposync --download-metadata -m \
+  --repo=$1 \
+  -p /mnt > $1.log &
+  pid=$!
+echo "Process $pid is running"
+
+while kill -0 $pid 2>/dev/null; do
+  sleep 15
+  echo -e "Free Mem: $(free -h | awk 'NR==2 {print $4}')" \
+    "\tFree Space: $(df -h /mnt | awk 'NR==2 {print $4}')" \
+    "\n$(tail -n 1 $1.log)"
+done
+
+echo "Process $pid is Complete"
+echo -e "\nListing:\n$(ls -l /mnt/$1)"
+echo -e "\nConsumption:\n$(du -sh /mnt/$1)"
