0c49a1a1d625b2e940c9d9a80b44e6d82cd045f9
John Pappas <John.Pappas@JacobsFederal.com>
2025-02-19 16:10:36 -0700

    Cleanup and add dockerfiles

--- registerubi.sh
+++ registerubi.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 VERSION=$(grep -oP '(?<=VERSION_ID=")[^.]+' /etc/os-release)
-rm /etc/rhsm-host
+rm -v /etc/rhsm-host /etc/yum.repos.d/ubi.repo
 subscription-manager register --username ${1} --password ${2}
 dnf install -y dnf-plugins-core yum-utils mkisofs isomd5sum tree procps-ng
 echo "Registering epel Repo"
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION}.noarch.rpm
 echo "Registering docker-ce-stable Repo"
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+echo "Registering hashicorp Repo"
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+echo "Instgalling Software"
+dnf install -y dnf-plugins-core yum-utils mkisofs isomd5sum tree procps-ng ncurses
