eff36e245df0ed2cee45f23901a0cbaf13455346
John Pappas <John.Pappas@JacobsFederal.com>
2025-02-19 13:02:23 -0700

    Add Hashicorp repo

--- registerubi.sh
+++ registerubi.sh
@@ -7,3 +7,4 @@ echo "Registering epel Repo"
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION}.noarch.rpm
 echo "Registering docker-ce-stable Repo"
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
