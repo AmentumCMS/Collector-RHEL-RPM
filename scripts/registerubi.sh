8a9b83518d27f10d640e9205c2bfeab90568a436
John Pappas <John.Pappas@JacobsFederal.com>
2025-02-20 15:42:40 -0700

    Clean up output

--- scripts/registerubi.sh
+++ scripts/registerubi.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 VERSION=$(grep -oP '(?<=VERSION_ID=")[^.]+' /etc/os-release)
+export PATH=$PATH:$PWD/scripts
 rm -v /etc/rhsm-host /etc/yum.repos.d/ubi.repo
 subscription-manager register --username ${1} --password ${2}
 dnf install -y dnf-plugins-core yum-utils mkisofs isomd5sum tree procps-ng
+echo "Registering Code Ready Builder Repo"
+/usr/bin/crb enable
 echo "Registering epel Repo"
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION}.noarch.rpm
 echo "Registering docker-ce-stable Repo"
@@ -11,5 +14,4 @@ echo "Registering hashicorp Repo"
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 echo "Installing Software"
 dnf install -y dnf-plugins-core yum-utils mkisofs isomd5sum tree procps-ng ncurses
-echo -e "\nAvailible Repositories:\n"
-dnf repolist
+echo -e "\nAvailable Repositories:\n$(dnf repolist)\n"
