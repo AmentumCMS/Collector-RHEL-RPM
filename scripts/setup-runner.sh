2592addd314d23ad4fc202049d1867ec71b7b870
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2025-03-11 08:09:23 -0600

    Create script config-runner.sh and prompt to use it.

--- scripts/setup-runner.sh
+++ scripts/setup-runner.sh
@@ -6,7 +6,3 @@ curl -o actions-runner-linux-x64-2.322.0.tar.gz -L https://github.com/actions/ru
 echo "b13b784808359f31bc79b08a191f5f83757852957dd8fe3dbfcc38202ccf5768  actions-runner-linux-x64-2.322.0.tar.gz" | shasum -a 256 -c
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.322.0.tar.gz
-# Create the runner and start the configuration experience
-./config.sh --url https://github.com/AmentumCMS --token $GITHUB_RUNNER_TOKEN
-# Last step, run it!
-./run.sh
