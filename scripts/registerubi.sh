247bfabff9af3dd0baf35bf486d44971296aadb9
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2025-03-26 22:52:10 -0600

    Add edge repo registration

--- scripts/registerubi.sh
+++ scripts/registerubi.sh
@@ -15,7 +15,10 @@ echo "Registering hashicorp Repo"
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 echo "Registering Microsoft Repo"
 dnf install -y https://packages.microsoft.com/config/rhel/${VERSION}/packages-microsoft-prod.rpm
+sed -i 's/packages-microsoft-com-prod/microsoft-prod/g' /etc/yum.repos.d/microsoft-prod.repo
 echo "Registering Microsoft Edge Repo"
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge.repo
+sed -i 's/packages.microsoft.com_yumrepos_edge/microsoft-edge/g' /etc/yum.repos.d/microsoft-edge.repo
 echo -e "\nAvailable Repositories:\n$(dnf repolist)\n"
