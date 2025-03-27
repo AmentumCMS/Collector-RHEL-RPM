ef0baa99e46baf82dfae2b687998a8675eebcf7d
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2025-03-26 22:24:19 -0600

    Update Codeready builder registration

--- scripts/registerubi.sh
+++ scripts/registerubi.sh
@@ -6,11 +6,13 @@ subscription-manager register --username ${1} --password ${2}
 echo "Installing Software"
 dnf install -y dnf-plugins-core yum-utils xorriso isomd5sum tree procps-ng ncurses
 echo "Registering Code Ready Builder Repo"
-/usr/bin/crb enable
+subscription-manager repos --enable codeready-builder-for-rhel-${VERSION}-x86_64-rpms
 echo "Registering epel Repo"
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION}.noarch.rpm
 echo "Registering docker-ce-stable Repo"
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 echo "Registering hashicorp Repo"
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+echo "Registering Microsoft Repo"
+dnf install -y https://packages.microsoft.com/config/rhel/${VERSION}/packages-microsoft-prod.rpm
 echo -e "\nAvailable Repositories:\n$(dnf repolist)\n"
